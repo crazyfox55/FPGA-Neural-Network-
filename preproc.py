1e58c49ef81cc33dec202adc139b38b755435a66
rpolley <ryan-polley@uiowa.edu>
2018-03-26 18:23:57 -0500

    did some things in the wrong order

--- preproc.py
+++ preproc.py
@@ -5,6 +5,6 @@ highres = cv2.VideoCapture(sys.argv[1])
 lowres = cv2.VideoWriter(sys.argv[2], cv2.VideoWriter_fourcc('f', 'm', 'p', '4'), 20, (1280,720), 1)
 success, hrframe = highres.read()
 while success:
-	success, hrframe = highres.read()
 	lrframe = cv2.resize(hrframe, (1280,720))
 	lowres.write(lrframe)
+	success, hrframe = highres.read()
